a3f0079373a0ba51d218a52c110d1242e2aadf86
cmcmahill24 <caitlin.mcmahill@yahoo.com>
2024-09-17 00:56:07 -0500

    Add files via upload

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,50 +1,68 @@
+DROP TABLE IF EXISTS "Campaign";
+DROP TABLE IF EXISTS "Category";
+DROP TABLE IF EXISTS "Contacts";
+DROP TABLE IF EXISTS "Subcategory";
+
 CREATE TABLE "Campaign" (
-    "cf_id" INT NOT NULL,
-    "contact_id" INT NOT NULL,
-    "company_name" VARCHAR(200) NOT NULL,
-    "description" VARCHAR(200) NOT NULL,
-    "goal" FLOAT NOT NULL,
-    "pledged" FLOAT NOT NULL,
-    "outcome" VARCHAR(20) NOT NULL,
-    "backers_count" INT NOT NULL,
-    "country" VARCHAR(2) NOT NULL,
-    "currency" VARCHAR(10) NOT NULL,
-    "launched_date" DATE NOT NULL,
-    "end_date" DATE NOT NULL,
-    "category_id" VARCHAR(10) NOT NULL,
-    "subcategory_id" VARCHAR(15) NOT NULL,
-    CONSTRAINT "pk_Campaign" PRIMARY KEY ("cf_id"),
-    CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id") REFERENCES "Contacts" ("contact_id"),
-    CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id") REFERENCES "Category" ("category_id"),
-    CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id") REFERENCES "Subcategory" ("subcategory_id")
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(200)   NOT NULL,
+    "description" VARCHAR(200)   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(20)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(2)   NOT NULL,
+    "currency" VARCHAR(10)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
+    "subcategory_id" VARCHAR(15)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
 );
 
 CREATE TABLE "Category" (
-    "category_id" VARCHAR(10) NOT NULL,
-    "category" VARCHAR(50) NOT NULL,
-    CONSTRAINT "pk_Category" PRIMARY KEY ("category_id")
+    "category_id" VARCHAR(10)   NOT NULL,
+    "category" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
 );
 
 CREATE TABLE "Contacts" (
-    "contact_id" INT NOT NULL,
-    "first_name" VARCHAR(30) NOT NULL,
-    "last_name" VARCHAR(50) NOT NULL,
-    "email" VARCHAR(100) NOT NULL,
-    CONSTRAINT "pk_Contacts" PRIMARY KEY ("contact_id")
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "email" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
 );
 
 CREATE TABLE "Subcategory" (
-    "subcategory_id" VARCHAR(15) NOT NULL,
-    "subcategory" VARCHAR(50) NOT NULL,
-    CONSTRAINT "pk_Subcategory" PRIMARY KEY ("subcategory_id")
+    "subcategory_id" VARCHAR(15)   NOT NULL,
+    "subcategory" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
 );
 
 -- Import CSV data into the tables
--- Adjust the file paths as necessary
-COPY "Campaign" FROM 'Resources/campaign_data.csv' DELIMITER ',' CSV HEADER;
-COPY "Category" FROM 'Resources/to/category_data.csv' DELIMITER ',' CSV HEADER;
-COPY "Contacts" FROM 'Resources/contacts_data.csv' DELIMITER ',' CSV HEADER;
-COPY "Subcategory" FROM 'Resources/subcategory_data.csv' DELIMITER ',' CSV HEADER;
+--COPY "Campaign" FROM '/path/to/campaign_data.csv' DELIMITER ',' CSV HEADER;
+--COPY "Category" FROM '/path/to/category_data.csv' DELIMITER ',' CSV HEADER;
+--COPY "Contacts" FROM '/path/to/contacts_data.csv' DELIMITER ',' CSV HEADER;
+--COPY "Subcategory" FROM '/path/to/subcategory_data.csv' DELIMITER ',' CSV HEADER;
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
 
 -- Display data from each table
 SELECT * FROM "Campaign";
